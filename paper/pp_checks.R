af73baa1374dace5a2ee6b2cddde08f9d2ecce64
Derek Powell <derekpowll@gmail.com>
2024-11-15 10:33:29 -0700

    resubmission

---
+++ paper/pp_checks.R
@@ -0,0 +1,12 @@
+pp_check(fit_imp_quiz_int, type = 'bars_grouped', group = 'implication', ndraws = 500)
+pp_check(fit_imp_quiz_int, type = 'bars_grouped', group = 'familiarity', ndraws = 500)
+pp_check(fit_imp_quiz_int, type = 'bars_grouped', group = 'B_implied_true', ndraws = 500)
+
+
+pp_check(fit_imp_exp2, type = 'bars_grouped', group = 'implication', ndraws = 500)
+pp_check(fit_imp_exp2, type = 'bars_grouped', group = 'familiarity', ndraws = 500)
+pp_check(fit_imp_exp2, type = 'bars_grouped', group = 'B_implied_true', ndraws = 500)
+
+pp_check(fit_imp_maximal3, type = 'bars_grouped', group = 'implication', ndraws = 500)
+pp_check(fit_imp_maximal3, type = 'bars_grouped', group = 'familiarity', ndraws = 500)
+pp_check(fit_imp_maximal3, type = 'bars_grouped', group = 'B_implied_true', ndraws = 500)
